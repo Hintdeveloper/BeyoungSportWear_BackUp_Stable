4c616b9a427ed45c71f2c30459677802e23cc32a
Hintdeveloper <117146376+Hintdeveloper@users.noreply.github.com>
2024-08-30 15:23:12 +0700

    Điều chỉnh một chút

--- BusinessLogicLayer/Viewmodels/CartProductDetails/CartOptionsVM.cs
+++ BusinessLogicLayer/Viewmodels/CartProductDetails/CartOptionsVM.cs
@@ -9,12 +9,13 @@ namespace BusinessLogicLayer.Viewmodels.CartProductDetails
     public class CartOptionsVM
     {
         public Guid IDOptions { get; set; }
-        public Guid IDCart { get; set; }
+        public string IDCart { get; set; }
+        public string ProductName { get; set; }
+        public string ImageURL { get; set; }
+        public string SizeName { get; set; }
+        public string ColorName { get; set; }
         public int Quantity { get; set; }
         public decimal UnitPrice { get; set; }
-
         public decimal TotalPrice { get; set; }
-
-        public int Status { get; set; }
     }
 }
