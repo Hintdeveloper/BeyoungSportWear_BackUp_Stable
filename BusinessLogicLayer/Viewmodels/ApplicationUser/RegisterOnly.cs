a8059d86c6f804c64d6d4980d0106f5a6fb51837
Hintdeveloper <117146376+Hintdeveloper@users.noreply.github.com>
2024-09-05 20:57:59 +0700

    Cập nhật data annotation cho ViewModel

--- BusinessLogicLayer/Viewmodels/ApplicationUser/RegisterOnly.cs
+++ BusinessLogicLayer/Viewmodels/ApplicationUser/RegisterOnly.cs
@@ -1,11 +1,21 @@
 ﻿
+using System.ComponentModel.DataAnnotations;
+
 namespace BusinessLogicLayer.Viewmodels.ApplicationUser
 {
     public class RegisterOnly
     {
+        [RegularExpression(@"^(\p{L}+\s){1,3}\p{L}+$", ErrorMessage = "Yêu cầu đầy đủ họ tên và nhập đúng định dạng, ví dụ: Nguyễn Văn A")]
+        [Required(ErrorMessage = "Họ và tên không được bỏ trống")]
+
         public string FirstAndLastName { get; set; } = null!;
+        [Required(ErrorMessage = "Tên người dùng không được bỏ trống")]
         public string Username { get; set; } = null!;
+        [RegularExpression(@"^[a-zA-Z0-9._%+-]+@[a-zA-Z.-]+\.[a-zA-Z]{2,}(?:\.[a-zA-Z]{2,})?$", ErrorMessage = "Yêu cầu nhập email đúng định dạng")]
+        [Required(ErrorMessage = "Email không được để trống")]
         public string Email { get; set; } = null!;
+        [RegularExpression(@"^0\d{9}$", ErrorMessage = "Số điện thoại phải bắt đầu bằng số 0 và gồm 10 chữ số.")]
+        [Required(ErrorMessage = "Số điện thoại không được để trống")]
         public string PhoneNumber { get; set; }
         public int Gender { get; set; }
     }
--- BusinessLogicLayer/Viewmodels/ApplicationUser/UserUpdateVM.cs
+++ BusinessLogicLayer/Viewmodels/ApplicationUser/UserUpdateVM.cs
@@ -15,10 +15,13 @@ namespace BusinessLogicLayer.Viewmodels.ApplicationUser
        
         public string? ModifiedBy { get; set; }
         [RegularExpression(@"^([a-zA-ZÀ-ỹ]+\s){2,3}[a-zA-ZÀ-ỹ]+$", ErrorMessage = "Yêu cầu đầy đủ họ tên và nhập đúng định dạng, ví dụ: Nguyễn Văn A")]
+        [Required(ErrorMessage = "Họ và tên không được bỏ trống")]
         public string? FirstAndLastName { get; set; }
         [RegularExpression(@"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}(?:\.[a-zA-Z]{2,})?$", ErrorMessage = "Yêu cầu nhập email đúng định dạng")]
+        [Required(ErrorMessage = "Email không được để trống")]
         public string? Email { get; set; }
         [RegularExpression(@"^0\d{9}$", ErrorMessage = "Số điện thoại phải bắt đầu bằng số 0 và gồm 10 chữ số.")]
+        [Required(ErrorMessage = "Số điện thoại không được để trống")]
         public string? PhoneNumber { get; set; }
         public int? Gender { get; set; }
         public IFormFile? Images { get; set; }
