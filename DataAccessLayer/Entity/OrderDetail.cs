bddac9696356f398e65cf3f5e8e9fb7f5072ec0c
Hintdeveloper <117146376+Hintdeveloper@users.noreply.github.com>
2024-09-09 21:33:44 +0700

    Đoạn này tôi vừa thử Scaffold không được nên undo hết, mà nó vẫn hiện nên tôi push, không có thay đổi gì

---
+++ DataAccessLayer/Entity/OrderDetail.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataAccessLayer.Entity;
+
+public partial class OrderDetail
+{
+    public Guid Id { get; set; }
+
+    public Guid Idorder { get; set; }
+
+    public Guid Idoptions { get; set; }
+
+    public int Quantity { get; set; }
+
+    public decimal UnitPrice { get; set; }
+
+    public decimal TotalAmount { get; set; }
+
+    public decimal? Discount { get; set; }
+
+    public string CreateBy { get; set; } = null!;
+
+    public DateTime CreateDate { get; set; }
+
+    public string? ModifiedBy { get; set; }
+
+    public DateTime? ModifiedDate { get; set; }
+
+    public string? DeleteBy { get; set; }
+
+    public DateTime? DeleteDate { get; set; }
+
+    public int Status { get; set; }
+
+    public virtual Options IdoptionsNavigation { get; set; } = null!;
+
+    public virtual Order IdorderNavigation { get; set; } = null!;
+}
